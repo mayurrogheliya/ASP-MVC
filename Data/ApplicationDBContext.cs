384eab89a0a16e8636608f0d2f7c2e5ad71f5185
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-08 23:42:20 +0530

    configure the database context and DbSet for student entity

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnderstandingMVC.Models.Entities;
+
+namespace UnderstandingMVC.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
