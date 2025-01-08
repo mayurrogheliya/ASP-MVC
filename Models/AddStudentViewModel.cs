dde1033ccbd08a57a2ce11fa2f11a6c5d4bddcfe
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-08 23:39:05 +0530

    created student model

---
+++ Models/AddStudentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace UnderstandingMVC.Models
+{
+    public class AddStudentViewModel
+    {
+         public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Branch { get; set; }
+        public int Semester { get; set; }
+        public double Result { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Models/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace UnderstandingMVC.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Branch { get; set; }
+        public int Semester { get; set; }
+        public double Result { get; set; }
+        public string Address { get; set; }
+    }
+}
