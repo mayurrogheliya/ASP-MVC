6f77faac4abf3dad942dee592dc09c7858fd4118
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-08 23:48:28 +0530

    remove the student action from HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace UnderstandingMVC.Controllers
             return View();
         }
 
-        public IActionResult Student()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
