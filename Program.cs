1706690e4bc663c2192549adcd2f68c30d8a1c00
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-08 23:45:56 +0530

    database configuration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using UnderstandingMVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("StudentPortal")));
 
 var app = builder.Build();
 
