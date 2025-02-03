149d1f8f7a02a7637c98218d11fd6bc0337c81ee
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-02-03 10:09:54 +0530

    implemented validation in AddStudentViewModel

--- Models/AddStudentViewModel.cs
+++ Models/AddStudentViewModel.cs
@@ -1,12 +1,25 @@
-﻿namespace UnderstandingMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UnderstandingMVC.Models
 {
     public class AddStudentViewModel
     {
+        [Required(ErrorMessage ="Please enter name")]
          public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter phone")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter branch")]
         public string Branch { get; set; }
+
+        [Required(ErrorMessage = "Please enter semester")]
         public int Semester { get; set; }
+
+        [Required(ErrorMessage = "Please enter result")]
         public double Result { get; set; }
+
+        [Required(ErrorMessage = "Please enter address")]
         public string Address { get; set; }
     }
 }
